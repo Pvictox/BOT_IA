7f607545ae604fefc24f8869bb09def40232dbdc
Walderney Oliveira Azevedo <85721450+wanoliveiraa@users.noreply.github.com>
2022-09-30 21:37:48 -0300

    boot simples

--- Bots.java
+++ Bots.java
@@ -1,6 +1,57 @@
 import java.util.ArrayList;
 
 public class Bots {
-    private int[] movimento; 
-    private ArrayList<int[]> 
+    private int [] movimento;
+    private int y;
+    private int x;
+    private int pontos;
+    public void agenteSimples(Campo campo){
+        y=campo.getPosicaoAtual()[0];
+        x=campo.getPosicaoAtual()[1];
+        if(pontos>0){
+            if(x>0){
+                campo.movLeft(); return;
+            }else if(y>0){
+                campo.movTop(); return;
+            }
+        }
+        
+        var aux=campo.checkLixo();
+        if(aux !=0) pontos=aux;
+        if(x==0 && y==0){
+            campo.setPontos(pontos+campo.getPontos());
+            this.pontos=0;
+        }
+       
+        if( x==19 && y%2==0){campo.movDown(); return;}
+        if (x==0 && y%2==1) {campo.movDown(); return;}
+        if(y%2==0 ) {campo.movRight(); return;}
+        if(y%2==1){ campo.movLeft(); return ; }
+       
+    }
+    public int[] getMovimento() {
+        return movimento;
+    }
+    public void setMovimento(int[] movimento) {
+        this.movimento = movimento;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getPontos() {
+        return pontos;
+    }
+    public void setPontos(int pontos) {
+        this.pontos = pontos;
+    }
 }
+  
--- app.java
+++ app.java
@@ -1,10 +1,21 @@
-public class app {
+public class App {
     public static void main(String[] args) {
         int[] posInicial = {0,0};
         Campo campo = new Campo(20, 20, posInicial);
+        Bots b = new Bots();
         campo.criar_Campo();
         int count = 1;
         System.out.println("size: "+campo.getLixos().size());
         campo.atualizaCampo();
+        
+        while(campo.getPontos()<150){
+            b.agenteSimples(campo);
+            System.out.println("\n\n\n\n\n\n\n");
+            campo.atualizaCampo();
+            System.out.println(campo.getPontos());
+            System.out.println(campo.getLixos().size());
+        }
+      
+        
     }
 }
